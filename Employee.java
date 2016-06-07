9ca9641e296da8a8a0e820565f8ca7a4390d3ca3
Anil Kumar <sendkumaranil@gmail.com>
2016-06-07 17:35:06 +0530

    POJO class of Employee Entity where relationship defined with annotataion

---
+++ Employee.java
@@ -0,0 +1,50 @@
+package com.hibernate.inheritance.tabpersubclass.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employees")
+@Inheritance(strategy=InheritanceType.JOINED)
+public class Employee {
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String org;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getOrg() {
+		return org;
+	}
+
+	public void setOrg(String org) {
+		this.org = org;
+	}
+	
+
+}
